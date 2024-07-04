99dd51691b7d6121de50cd67562b166c2daa91b0
SYEDA ARBISH <jugnoo708@gmail.com>
2024-07-04 17:18:48 +0500

    Add files via upload

---
+++ TRIANGLE MESUREMENT/MAIN.PY/TRI.py
@@ -0,0 +1,10 @@
+side1=int(input("enter the side1"))
+side2=int(input("enter the side2"))
+side3=int(input("enter the side3"))
+#determine the type of triangle
+if side1==side2==side3 :
+    print("EQUILATERAL TRIANGLE")
+elif side1 == side2 or side2 == side3 or side1 ==side3:
+    print("ISOSCELES TRIANLGE")
+else:
+    print("SCALENE TRIANGLE")
